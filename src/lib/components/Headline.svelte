b13f3396c4797064cca7d5e2b26b64d2e4fd5dbc
Dan Hahn <daniel.hahn@onepeloton.com>
2023-02-15 11:29:51 -0500

    add butterflys

--- src/lib/components/Headline.svelte
+++ src/lib/components/Headline.svelte
@@ -1,11 +1,20 @@
 <script>
 	import Section from './Section.svelte';
+	import Stack from './Stack.svelte';
 </script>
 
 <Section>
-	<h1
-		class={`font-display text-center text-3xl lg:text-5xl text-megan-800 border-b border-b-megan-900 pb-4 ${$$props.class}`}
-	>
-		<slot />
-	</h1>
+	<Stack>
+		<img
+			src="/butterfly.svg"
+			alt="butterfly"
+			class="opacity-20 z-10 w-16 mx-auto rotate-12 -translate-y-3"
+		/>
+
+		<h1
+			class={`font-display text-center text-3xl lg:text-5xl text-megan-800 border-b border-b-megan-900 pb-4 z-20 ${$$props.class}`}
+		>
+			<slot />
+		</h1>
+	</Stack>
 </Section>
--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -62,7 +62,7 @@
 		<img
 			src="/butterfly.svg"
 			alt="butterfly"
-			class="opacity-40 z-10 -translate-x-4 -rotate-12  scale-50"
+			class="opacity-40 z-10 -translate-x-2 -translate-y-6 rotate-12  scale-50 absolute"
 		/>
 		<div class={`sm-logo normal-case font-display text-2xl z-20 ${$$props.class}`}>
 			<span>M<span class="restName opacity-0">eagan</span></span>
