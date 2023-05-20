4ad26b1e8ae051c705d4b7dc0277b3c86c4f25ec
Dan Hahn <danielhahn@gmail.com>
2023-05-20 11:16:13 -0400

    adjust input spacing

--- src/lib/components/forms/Input.svelte
+++ src/lib/components/forms/Input.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <div class="relative w-96">
-	<label class="transition-all absolute left-4 top-[9px]" class:isActive for={id}>{label}</label>
+	<label class="transition-all absolute left-4 top-[13px]" class:isActive for={id}>{label}</label>
 	<input
 		{...$$props}
 		{id}
@@ -22,7 +22,7 @@
 		on:focus={handleFocus}
 		on:blur={handleBlur}
 		class:isActive
-		class="transition-all bg-white rounded-md py-2 px-4 border-2 w-full border-gray-400 focus:outline-none focus:shadow-outline-purple focus:border-megan-500"
+		class="bg-white rounded-md py-3 px-4 border-2 w-full border-gray-400 focus:outline-none focus:border-megan-500"
 	/>
 </div>
 
@@ -32,6 +32,6 @@
 	}
 
 	input.isActive {
-		@apply pt-[10px] pb-[6px];
+		@apply pt-[18px] pb-[6px];
 	}
 </style>
