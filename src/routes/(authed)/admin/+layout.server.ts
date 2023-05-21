09cbff3326ddf66dbf7fe61a8a40071f14c9e2de
Dan Hahn <danielhahn@gmail.com>
2023-05-21 18:02:09 -0400

    remove server pages

--- src/routes/(authed)/admin/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
-
-export const load = (({ cookies, url }) => {
-	// if (!cookies.get('logged_in')) {
-	// 	throw redirect(303, `/login?redirectTo=${url.pathname}`);
-	// }
-	console.log('load');
-}) satisfies LayoutServerLoad;
