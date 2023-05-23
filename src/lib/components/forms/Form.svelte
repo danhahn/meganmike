9a1f17de416c9fdae2f977688471dc47b935761a
Dan Hahn <daniel.hahn@onepeloton.com>
2023-05-23 17:25:06 -0400

    adjust the form to display correct

--- src/lib/components/forms/Form.svelte
+++ src/lib/components/forms/Form.svelte
@@ -2,14 +2,14 @@
 	export let size: 'full' | 'small' = 'full';
 	export let centered: boolean = true;
 
-	const sizeClass = size === 'full' ? 'w-full' : 'min-w-[60%]';
+	const sizeClass = size === 'full' ? 'w-full' : 'w-[80%] md:w-[50%] lg:max-w-[30%]';
 	const centeredClass = centered ? 'mx-auto' : '';
 	const buttonAlign = size === 'full' ? 'flex-end' : 'auto';
 </script>
 
 <form
 	{...$$props}
-	class={`flex flex-col gap-2 sm:gap-4 ${sizeClass} ${centeredClass}`}
+	class={`flex flex-col gap-2 sm:gap-4 text-center lg:text-start mb-32 md:mb-64 lg:mb-0 ${sizeClass} ${centeredClass}`}
 	style={`--align: ${buttonAlign}`}
 	on:submit|preventDefault
 >
