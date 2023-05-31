f9005c606f681004803df0d0ff5d9e53566d9384
Dan Hahn <daniel.hahn@onepeloton.com>
2023-05-31 12:00:56 -0400

    add background to admin page

--- src/routes/(authed)/+layout@.svelte
+++ src/routes/(authed)/+layout@.svelte
@@ -46,7 +46,7 @@
 		<Header />
 	{/if}
 
-	<div class="grid grid-rows-[auto_1fr]">
+	<div class="main grid grid-rows-[auto_1fr]">
 		<div
 			class="px-4 py-2 bg-megan-900 text-megan-100 grid grid-cols-[auto_auto_1fr] lg:grid-cols-[auto_1fr] gap-4 items-center"
 		>
@@ -71,6 +71,24 @@
 </section>
 
 <style lang="postcss">
+	section {
+		--s: 50px; /* control the size */
+		--c1: hsl(48 27% 85% / 1);
+		--c2: hsl(49 14% 52% / 1);
+
+		--_g: #0000, #0004 5%, var(--c2) 6% 14%, var(--c1) 16% 24%, var(--c2) 26% 34%, var(--c1) 36% 44%,
+			var(--c2) 46% 54%, var(--c1) 56% 64%, var(--c2) 66% 74%, var(--c1) 76% 84%, var(--c2) 86% 94%,
+			#0004 95%, #0000;
+		background: radial-gradient(100% 50% at 100% 0, var(--_g)),
+			radial-gradient(100% 50% at 0 50%, var(--_g)),
+			radial-gradient(100% 50% at 100% 100%, var(--_g));
+		background-size: var(--s) calc(2 * var(--s));
+	}
+
+	.main {
+		@apply bg-white/80;
+	}
+
 	.notLoggedIn {
 		@apply lg:mt-24;
 	}
