a112c613a044fdec286abc8d0a93002ee7d01383
Dan Hahn <daniel.hahn@onepeloton.com>
2023-06-05 17:38:16 -0400

    add new component

---
+++ src/lib/components/admin/Pagination.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Button from '../forms/Button.svelte';
+
+	export let total: number;
+	export let current: number;
+	export let docsPerPage: number;
+
+	$: numberOfPages = total / docsPerPage;
+</script>
+
+<div class="flex justify-center mt-4">
+	<Button size="small">Back</Button>
+	{#each Array.from({ length: numberOfPages }) as page, index}
+		<Button size="small" variant={current === index ? 'secondary' : 'primary'}>{index + 1}</Button>
+	{/each}
+	<Button size="small">next</Button>
+</div>
