d27a85ad4612ff7e8b0eafdb26acef87499d091d
Dan Hahn <daniel.hahn@onepeloton.com>
2023-07-06 12:52:29 -0400

    upate gallery

--- src/routes/(site)/gallery/+page.svelte
+++ src/routes/(site)/gallery/+page.svelte
@@ -4,7 +4,27 @@
 	import Section from '$lib/components/Section.svelte';
 	import { title } from '$lib/utils';
 
-	const items = Array.from({ length: 12 }).map((_, index) => index + 1);
+	const items = [
+		'engagment/water.jpg',
+		'engagment/kiss.jpg',
+		'engagment/snuggle.jpg',
+		'engagment/hero.jpg',
+		'engagment/theshot.jpg',
+		'engagment/dip.jpg',
+		'engagment/love.jpg',
+		'gallery1.jpg',
+		'gallery2.jpg',
+		'gallery3.jpg',
+		'gallery4.jpg',
+		'gallery5.jpg',
+		'gallery6.jpg',
+		'gallery7.jpg',
+		'gallery8.jpg',
+		'gallery9.jpg',
+		'gallery10.jpg',
+		'gallery11.jpg',
+		'gallery12.jpg'
+	];
 </script>
 
 <svelte:head>
@@ -15,13 +35,7 @@
 <Section>
 	<div class="grid gap-2 p-2 grid-cols-1 sm:grid-cols-2">
 		{#each items as item}
-			<Img
-				src={`gallery${item}.jpg`}
-				alt="All smiles"
-				gravity="faces"
-				size="square"
-				class="rounded-xl"
-			/>
+			<Img src={item} alt="" gravity="faces" size="square" class="rounded-xl" />
 		{/each}
 	</div>
 </Section>
