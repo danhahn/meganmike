1abbcd9640e52e14349a587c2ef4a171bd857ed5
Dan Hahn <daniel.hahn@onepeloton.com>
2024-01-09 13:29:54 -0500

    Add Firebase integration to gallery layout component

---
+++ src/routes/(site)/gallery/[id]/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { FirebaseApp } from 'sveltefire';
+	import { initializeApp } from 'firebase/app';
+	import { getFirestore } from 'firebase/firestore';
+	import { getAuth } from 'firebase/auth';
+	import { auth, firestore, storage } from '$lib/firebase/firebase';
+</script>
+
+<FirebaseApp {auth} {firestore} {storage}>
+	<slot />
+</FirebaseApp>
