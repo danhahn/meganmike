9b4cc1e9f4b5e6d080458f97b6db8b0c5f6bb705
Dan Hahn <danielhahn@gmail.com>
2024-01-08 22:47:04 -0500

    Add new page for uploading photos to the gallery

---
+++ src/routes/(site)/gallery/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Headline from '$lib/components/Headline.svelte';
+	import Section from '$lib/components/Section.svelte';
+	import type { PageData } from './$types';
+
+	let files: FileList;
+
+	export let data: PageData;
+
+	function handleUpload() {
+		console.log(files);
+	}
+</script>
+
+<Headline>Add Photos to the gallery</Headline>
+<Section>
+	<p>Share your photos from todays event</p>
+
+	<input type="file" multiple accept="image/*" bind:value={files} on:change={handleUpload} />
+
+	<p>{data.id}</p>
+</Section>
