c1d038e33248c9df0c858ccff87db3580a54cc0f
Dan Hahn <daniel.hahn@onepeloton.com>
2024-01-09 13:30:02 -0500

    Add file upload functionality to gallery page

--- src/routes/(site)/gallery/[id]/+page.svelte
+++ src/routes/(site)/gallery/[id]/+page.svelte
@@ -2,21 +2,72 @@
 	import Headline from '$lib/components/Headline.svelte';
 	import Section from '$lib/components/Section.svelte';
 	import type { PageData } from './$types';
+	import { DownloadURL, UploadTask, collectionStore } from 'sveltefire';
+
+	import { collection, where, query } from 'firebase/firestore';
+	import { firestore } from '$lib/firebase/firebase';
+	import { set } from 'firebase/database';
+	import Button from '$lib/components/forms/Button.svelte';
+	export let data: PageData;
+
+	const postsRef = collection(firestore, 'galleries');
+	const q = query(postsRef, where('name', '==', data.id));
+
+	const gallery = collectionStore(firestore, q);
+
+	$: isValidGallery = $gallery && $gallery.length === 1;
 
 	let files: FileList;
 
-	export let data: PageData;
+	let file: File | undefined;
 
-	function handleUpload() {
-		console.log(files);
+	let uploader: HTMLInputElement;
+
+	function chooseFile(e: Event) {
+		file = (e.target as HTMLInputElement).files?.[0];
+	}
+
+	function resetUpload() {
+		file = undefined;
+		uploader.value = '';
 	}
 </script>
 
-<Headline>Add Photos to the gallery</Headline>
-<Section>
-	<p>Share your photos from todays event</p>
+<svelte:head>
+	<title>Gallery | {data.id}</title>
+</svelte:head>
 
-	<input type="file" multiple accept="image/*" bind:value={files} on:change={handleUpload} />
+{#if !isValidGallery}
+	<Headline>Gallery not found</Headline>
+	<Section>
+		<p>Sorry, we couldn't find the gallery you were looking for.</p>
+	</Section>
+{:else}
+	<Headline>Add Photos to the gallery</Headline>
 
-	<p>{data.id}</p>
-</Section>
+	<Section>
+		<p>Share your photos from todays event</p>
+
+		<input bind:this={uploader} type="file" accept="image/*" on:change={chooseFile} />
+
+		<p>{data.id}</p>
+
+		{#if file}
+			<UploadTask ref={`${data.id}/${file.name}`} data={file} let:progress let:snapshot>
+				{#if snapshot?.state === 'running'}
+					<progress value={progress} max="100">{progress}%</progress>
+					{progress.toFixed(2)}% uploaded
+				{/if}
+
+				{#if snapshot?.state === 'success'}
+					<Button on:click={resetUpload}>Reset</Button>
+					<DownloadURL ref={snapshot?.ref} let:link>
+						<a href={link} download>
+							<img src={link} alt="" />
+						</a>
+					</DownloadURL>
+				{/if}
+			</UploadTask>
+		{/if}
+	</Section>
+{/if}
