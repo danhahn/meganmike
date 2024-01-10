9b4ae10e0aa86566929d32af2dd8b848b6b4bd58
Dan Hahn <daniel.hahn@onepeloton.com>
2024-01-10 12:13:49 -0500

    Update gallery page with photo upload functionality

--- src/routes/(site)/gallery/[id]/+page.svelte
+++ src/routes/(site)/gallery/[id]/+page.svelte
@@ -2,7 +2,7 @@
 	import Headline from '$lib/components/Headline.svelte';
 	import Section from '$lib/components/Section.svelte';
 	import type { PageData } from './$types';
-	import { DownloadURL, UploadTask, collectionStore } from 'sveltefire';
+	import { DownloadURL, StorageList, UploadTask, collectionStore } from 'sveltefire';
 
 	import { collection, where, query } from 'firebase/firestore';
 	import { firestore } from '$lib/firebase/firebase';
@@ -17,7 +17,7 @@
 
 	$: isValidGallery = $gallery && $gallery.length === 1;
 
-	let files: FileList;
+	let image = '';
 
 	let file: File | undefined;
 
@@ -25,6 +25,7 @@
 
 	function chooseFile(e: Event) {
 		file = (e.target as HTMLInputElement).files?.[0];
+		setTimeout(resetUpload, 2000);
 	}
 
 	function resetUpload() {
@@ -43,16 +44,11 @@
 		<p>Sorry, we couldn't find the gallery you were looking for.</p>
 	</Section>
 {:else}
-	<Headline>Add Photos to the gallery</Headline>
+	<Headline>Add a photo to todays event</Headline>
 
 	<Section>
-		<p>Share your photos from todays event</p>
-
-		<input bind:this={uploader} type="file" accept="image/*" on:change={chooseFile} />
-
-		<p>{data.id}</p>
-
 		{#if file}
+			<h2>Uploading {file.name}</h2>
 			<UploadTask ref={`${data.id}/${file.name}`} data={file} let:progress let:snapshot>
 				{#if snapshot?.state === 'running'}
 					<progress value={progress} max="100">{progress}%</progress>
@@ -60,14 +56,79 @@
 				{/if}
 
 				{#if snapshot?.state === 'success'}
-					<Button on:click={resetUpload}>Reset</Button>
 					<DownloadURL ref={snapshot?.ref} let:link>
 						<a href={link} download>
 							<img src={link} alt="" />
 						</a>
 					</DownloadURL>
+					<Button on:click={resetUpload}>Upload an other photo</Button>
 				{/if}
 			</UploadTask>
+		{:else}
+			<p>Please upload a any photos you have from todays event to share with Megan and Mike.</p>
+
+			<input
+				class="file-input"
+				id="file_input"
+				bind:this={uploader}
+				type="file"
+				accept="image/*"
+				on:change={chooseFile}
+			/>
 		{/if}
+
+		{#if image}
+			<img src={image} alt="" />
+		{/if}
+
+		<StorageList ref={data.id} let:list>
+			{#if list === null}
+				<p>Loading...</p>
+			{:else if list.prefixes.length === 0 && list.items.length === 0}
+				<li>Empty</li>
+			{:else}
+				<ul class="grid grid-cols-4 gap-4">
+					<!-- Listing the prefixes -->
+					{#each list.prefixes as prefix}
+						<li>
+							{prefix.name}
+						</li>
+					{/each}
+					<!-- Listing the objects in the given folder -->
+					{#each list.items as item}
+						<li class="flex flex-col gap-2">
+							<DownloadURL ref={`${data.id}/${item.name}`} let:link let:ref>
+								<!-- show img -->
+								{#if link}
+									<button on:click={() => (image = link)}>
+										<img src={link} alt={item.name} class="aspect-square object-cover" />
+									</button>
+
+									<!-- or download via link -->
+									<Button size="small" href={link} download>Download</Button>
+								{/if}
+							</DownloadURL>
+						</li>
+					{/each}
+				</ul>
+			{/if}
+		</StorageList>
 	</Section>
 {/if}
+
+<style lang="postcss">
+	.file-input {
+		@apply border-megan-400 bg-megan-100 rounded-xl overflow-hidden border border-opacity-0 text-base;
+	}
+
+	.file-input::file-selector-button {
+		border-style: solid;
+		@apply p-4 px-8 mr-4 border-megan-300 bg-megan-300 text-megan-800 font-semibold  no-underline;
+		border-width: var(--border-btn, 1px);
+		animation: button-pop var(--animation-btn, 0.25s) ease-out;
+	}
+
+	.file-input::file-selector-button:hover {
+		@apply bg-megan-400 border-megan-400;
+	}
+</style>
