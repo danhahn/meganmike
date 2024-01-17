7d00c08b0564bc239d314e7e59d2323c867c50b0
Dan <danielhahn@gmail.com>
2024-01-17 13:42:26 -0500

    Refactor gift registry page layout

--- src/routes/(site)/gift-registry/+page.svelte
+++ src/routes/(site)/gift-registry/+page.svelte
@@ -17,12 +17,39 @@
 <Section>
 	<h2>We are registered at</h2>
 
-	<img src="/amazon.svg" alt="Amazon logo" class="my-10" />
-
-	<Button
-		target="_blank"
-		rel="noreferrer"
-		href="https://www.amazon.com/wedding/registry/36N1N7K2MD39N?ref=wr_search_page_result_1"
-		>Link to Amazon</Button
-	>
+	<div class="border border-megan-600 p-6 rounded-lg bg-white">
+		<img src="/amazon.svg" alt="Amazon logo" class="my-10 w-full" />
+		<Button
+			target="_blank"
+			rel="noreferrer"
+			href="https://www.amazon.com/wedding/registry/36N1N7K2MD39N?ref=wr_search_page_result_1"
+			>Shop Amazon</Button
+		>
+	</div>
+	<div class="border border-megan-600 p-6 rounded-lg bg-white">
+		<img src="/CrateandBarrelLogo.svg" alt="Create and Barrel" class="my-10 w-full" />
+		<Button
+			target="_blank"
+			rel="noreferrer"
+			href="https://www.crateandbarrel.com/gift-registry/megan-iasenza/r6753029"
+			>Shop Create and Barrel</Button
+		>
+	</div>
+	<div class="border border-megan-600 p-6 rounded-lg bg-white">
+		<img
+			src="https://shop.water.com/media/logo/stores/1/Primo_Logo_Web.png"
+			alt="Create and Barrel"
+			class="my-10 w-1/4 text-center block mx-auto"
+		/>
+		<Button target="_blank" rel="noreferrer" href="https://shop.water.com/smart-touch"
+			>Shop Create and Barrel</Button
+		>
+		<p class="text-sm mt-4 flex gap-2 mx-4">
+			<span class="translate-y-1">*</span>
+			<span>
+				note this is not a registry. Please text Tracy if you are interested in giving this a gift
+				to see its availability.
+			</span>
+		</p>
+	</div>
 </Section>
