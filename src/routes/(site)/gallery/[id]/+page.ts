2ba8b766465b86c9c180898e1d023fc77f496638
Dan <danielhahn@gmail.com>
2024-03-16 16:53:19 -0400

    Add gallery validation in page load

--- src/routes/(site)/gallery/[id]/+page.ts
+++ src/routes/(site)/gallery/[id]/+page.ts
@@ -1,6 +1,15 @@
+import { collection, where, query, getDocs } from 'firebase/firestore';
+
+import { db } from '$lib/firebase/firebase';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
 	const { id } = params;
-	return { id };
+
+	// check if the id is a valid gallery from firestore
+	const galleryRef = query(collection(db, 'galleries'), where('name', '==', id));
+
+	const gallery = await getDocs(galleryRef);
+
+	return { id, status: !gallery.empty ? 'idle' : 'error' };
 }) satisfies PageLoad;
