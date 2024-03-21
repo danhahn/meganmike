a23edb7f3618a9b9e85f8ce8e7797394c1b33288
Dan <danielhahn@gmail.com>
2024-03-21 17:46:44 -0400

    Add gallery page with dynamic routing and image display

---
+++ src/routes/(site)/gallery/[id]/[photoId]/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	$: innerWidth = 1000;
+
+	$: imageSize = innerWidth > 768 ? 1000 : innerWidth;
+</script>
+
+<svelte:head>
+	<title>Gallery | {data.id} | {data.currentPhoto?.name || ''}</title>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+</svelte:head>
+<svelte:window bind:innerWidth />
+
+{#if data.status === 404}
+	<p>Page Not found</p>
+{:else}
+	<button class="fixed z-50 top-6 left-2" on:click={() => goto(`/gallery/${data.id}`)}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 -960 960 960"
+			class="w-6 h-6 fill-white translate-x-1"
+		>
+			<path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+		</svg>
+	</button>
+	<div class="h-full grid place-content-center fixed bg-megan-400 inset-0">
+		<div class="grid image-stack">
+			<img
+				src={`${data.currentPhoto?.url}&tr=w-${imageSize},h-${imageSize},c-at_least`}
+				alt=""
+				width={imageSize}
+				class="max-h-screen object-cover shadow-lg shadow-black/40"
+			/>
+			{#if data.nextPhoto}
+				<div class="self-center flex justify-end px-6 z-50">
+					<button
+						on:click={() => {
+							goto(`/gallery/${data.id}/${data.nextPhoto?.id}`);
+						}}
+						class="bg-megan-600/60 aspect-square w-12 grid place-content-center text-white rounded-full"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 -960 960 960"
+							class="w-6 h-6 fill-current translate-x-[1px]"
+						>
+							<path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
+						</svg>
+					</button>
+				</div>
+			{:else}
+				<div />
+			{/if}
+		</div>
+	</div>
+{/if}
+
+<style lang="postcss">
+	.image-stack > * {
+		grid-column: 1 / -1;
+		grid-row: 1 / -1;
+	}
+</style>
---
+++ src/routes/(site)/gallery/[id]/[photoId]/+page.ts
@@ -0,0 +1,51 @@
+import { db } from '$lib/firebase/firebase';
+import { query, collection, getDocs, doc, getDoc, startAt, limit } from 'firebase/firestore';
+import type { PageLoad } from './$types';
+
+type Photo = {
+	id: string;
+	url: string;
+	uploadedBy: string;
+	likes: number;
+	name: string;
+};
+
+export const load = (async ({ params }) => {
+	const { id, photoId } = params;
+
+	const imagesRef = collection(db, 'photos');
+
+	const photoRef = await getDoc(doc(imagesRef, photoId));
+
+	// check if photoRef exists
+	if (!photoRef.exists()) {
+		return {
+			status: 404,
+			error: 'Photo not found'
+		};
+	}
+
+	const test = query(imagesRef, startAt(photoRef), limit(2));
+
+	const nextSnap = await getDocs(test);
+
+	const currentPhots: Photo[] = [];
+
+	nextSnap.forEach((doc) => {
+		currentPhots.push({
+			id: doc.id,
+			url: doc.data().url,
+			uploadedBy: doc.data().uploadedBy,
+			likes: doc.data().likes,
+			name: doc.data().name
+		});
+	});
+
+	const [currentPhoto, nextPhoto] = currentPhots;
+
+	return {
+		currentPhoto,
+		nextPhoto,
+		id
+	};
+}) satisfies PageLoad;
