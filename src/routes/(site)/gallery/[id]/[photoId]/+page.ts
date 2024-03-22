62e6e422eae7fe04c0e4afbc7bface580ac91155
Dan <danielhahn@gmail.com>
2024-03-22 16:32:03 -0400

    Refactor gallery page load function

--- src/routes/(site)/gallery/[id]/[photoId]/+page.ts
+++ src/routes/(site)/gallery/[id]/[photoId]/+page.ts
@@ -1,23 +1,6 @@
 import { db } from '$lib/firebase/firebase';
-import {
-	query,
-	collection,
-	getDocs,
-	doc,
-	getDoc,
-	startAt,
-	limit,
-	orderBy
-} from 'firebase/firestore';
+import { collection, doc, getDoc } from 'firebase/firestore';
 import type { PageLoad } from './$types';
-
-type Photo = {
-	id: string;
-	url: string;
-	uploadedBy: string;
-	likes: number;
-	name: string;
-};
 
 export const load = (async ({ params }) => {
 	const { id, photoId } = params;
@@ -34,27 +17,7 @@ export const load = (async ({ params }) => {
 		};
 	}
 
-	const test = query(imagesRef, orderBy('dateAdded', 'desc'), startAt(photoRef), limit(2));
-
-	const nextSnap = await getDocs(test);
-
-	const currentPhots: Photo[] = [];
-
-	nextSnap.forEach((doc) => {
-		currentPhots.push({
-			id: doc.id,
-			url: doc.data().url,
-			uploadedBy: doc.data().uploadedBy,
-			likes: doc.data().likes,
-			name: doc.data().name
-		});
-	});
-
-	const [currentPhoto, nextPhoto] = currentPhots;
-
 	return {
-		currentPhoto,
-		nextPhoto,
 		id
 	};
 }) satisfies PageLoad;
