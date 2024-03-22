4d4b770a7952015f21ae01a5d3b34685d3e8bd7a
Dan <danielhahn@gmail.com>
2024-03-22 14:39:10 -0400

    Add gallery layout and query photos

--- src/routes/(site)/gallery/[id]/+layout.svelte
+++ src/routes/(site)/gallery/[id]/+layout.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
-	import { FirebaseApp } from 'sveltefire';
+	import { FirebaseApp, collectionStore } from 'sveltefire';
 	import { auth, firestore, storage } from '$lib/firebase/firebase';
+	import type { LayoutData } from './$types';
+	import { gallery, galleryId } from '$lib/stores/galleryStore';
+	import { collection, orderBy, query, where } from 'firebase/firestore';
+	import type { Image } from '$lib/types';
+	import { sortDirectionStore, sortFieldStore } from '$lib/stores/sortStore';
+
+	export let data: LayoutData;
+
+	const photosQuery = query(
+		collection(firestore, 'photos'),
+		where('gallery', '==', data.id),
+		orderBy($sortFieldStore, $sortDirectionStore)
+	);
+
+	const photos = collectionStore<Image>(firestore, photosQuery as any);
+
+	$: gallery.set($photos);
+	$: galleryId.set(data.id);
 </script>
 
 <FirebaseApp {auth} {firestore} {storage}>
