6ef1b6ab95fd2a546831a4d776303b7ab3e7ec73
Dan <danielhahn@gmail.com>
2024-03-27 18:28:46 -0400

    Refactor photosQuery in +layout.svelte

--- src/routes/(site)/gallery/[id]/+layout.svelte
+++ src/routes/(site)/gallery/[id]/+layout.svelte
@@ -3,17 +3,22 @@
 	import { auth, firestore, storage } from '$lib/firebase/firebase';
 	import type { LayoutData } from './$types';
 	import { gallery, galleryId } from '$lib/stores/galleryStore';
+	import type { DocumentData } from 'firebase/firestore';
 	import { collection, orderBy, query, where } from 'firebase/firestore';
 	import type { Image } from '$lib/types';
 	import { sortDirectionStore, sortFieldStore } from '$lib/stores/sortStore';
 
 	export let data: LayoutData;
 
-	let photosQuery = query(
-		collection(firestore, 'photos'),
-		where('gallery', '==', data.id),
-		orderBy($sortFieldStore, $sortDirectionStore)
-	);
+	let photosQuery: any;
+
+	if (data.id) {
+		photosQuery = query(
+			collection(firestore, 'photos'),
+			where('gallery', '==', data.id),
+			orderBy($sortFieldStore, $sortDirectionStore)
+		);
+	}
 
 	$: {
 		photosQuery = query(
