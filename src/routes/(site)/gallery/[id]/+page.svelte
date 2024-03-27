869b8522415acc4ebf242179c8deeda532f1f71f
Dan <danielhahn@gmail.com>
2024-03-27 18:28:54 -0400

    Fix bug in login functionality

--- src/routes/(site)/gallery/[id]/+page.svelte
+++ src/routes/(site)/gallery/[id]/+page.svelte
@@ -16,6 +16,7 @@
 	import { gallery } from '$lib/stores/galleryStore';
 	import { sortDirectionStore, sortFieldStore } from '$lib/stores/sortStore';
 	import viewport from '$lib/useViewportAction';
+	import InfoHeader from '$lib/components/InfoHeader.svelte';
 
 	export let data: PageData;
 
@@ -156,105 +157,54 @@
 </script>
 
 <svelte:head>
-	<title>Gallery | {data.title}</title>
-	<link
-		rel="stylesheet"
-		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-	/>
+	<title>Gallery | {data.title || 'Not Found'}</title>
 </svelte:head>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-<div class="min-h-full grid 🔥">
-	{#if status === 'loading'}
+<div class=" 🔥">
+	{#if data.status === 'error'}
+		<div class="mt-[20vh]">
+			<InfoHeader />
+			<div
+				class="grid justify-center text-center border-2 border-megan-500 max-w-sm mx-auto rounded-md bg-megan-50 p-5"
+			>
+				<h2 class="mb-2">Sorry</h2>
+				<p class="text-2xl uppercase">{data.message}</p>
+				<p class="mt-4">
+					<a href="/" class="flex justify-center items-center gap-2 -translate-x-2">
+						<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"
+							><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" /></svg
+						>
+						Back Home</a
+					>
+				</p>
+			</div>
+		</div>
+	{:else if status === 'loading'}
 		<p>Loading...</p>
 	{:else if status === 'idle'}
 		<div class="grid grid-rows-[auto_1fr] relative">
-			<div class="p-4 bg-megan-300/35 text-center text-megan-700 grid grid-cols-[32px_1fr_32px]">
-				<div class="whitespace-nowrap">
-					<button bind:this={sortButton} on:click={toggleDropdown}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 -960 960 960"
-							class="w-6 h-6 fill-megan-700"
-							><path
-								d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"
-							/></svg
-						>
-					</button>
+			<div
+				class="px-4 py-1 bg-megan-300/35 text-center text-megan-700 grid grid-cols-[auto_1fr_auto]"
+			>
+				<button bind:this={sortButton} on:click={toggleDropdown}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 -960 960 960"
+						class="w-4 h-4 fill-megan-700 translate-y-[1px]"
+						><path
+							d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"
+						/></svg
+					>
+				</button>
 
-					<dialog bind:this={sortDialog} class="absolute">
-						<div
-							class="relative bg-white text-left p-4 rounded-md shadow-lg z-[1001] lg:flex lg:gap-4 items-center"
-						>
-							<p class="text-md text-gray-600">Sort By</p>
-							<div class="bg-gray-100 grid grid-cols-2 gap-2 p-1">
-								<button
-									on:click={() => {
-										if ($sortFieldStore !== 'dateAdded') {
-											sortFieldStore.set('dateAdded');
-											sortDialog.close();
-										}
-									}}
-									class="bg-gray-300 rounded-md py-2 px-4 text-black"
-									class:selected={$sortFieldStore === 'dateAdded'}>Date Added</button
-								>
-								<button
-									on:click={() => {
-										if ($sortFieldStore !== 'dateTaken') {
-											sortFieldStore.set('dateTaken');
-											sortDialog.close();
-										}
-									}}
-									class="bg-gray-300 rounded-md py-2 px-4 text-black"
-									class:selected={$sortFieldStore === 'dateTaken'}>Date Taken</button
-								>
-							</div>
-							<p class="text-md mt-4 lg:mt-0 text-gray-600">Direction</p>
-							<div class="bg-gray-100 grid grid-cols-2 gap-2 p-1">
-								<button
-									on:click={() => {
-										if ($sortDirectionStore !== 'desc') {
-											sortDirectionStore.set('desc');
-											sortDialog.close();
-										}
-									}}
-									class="bg-gray-300 rounded-md py-2 px-4 text-black"
-									class:selected={$sortDirectionStore === 'desc'}>Newest</button
-								>
-								<button
-									on:click={() => {
-										if ($sortDirectionStore !== 'asc') {
-											sortDirectionStore.set('asc');
-											sortDialog.close();
-										}
-									}}
-									class="bg-gray-300 rounded-md py-2 px-4 text-black"
-									class:selected={$sortDirectionStore === 'asc'}>Oldest</button
-								>
-							</div>
-							<button
-								on:click={() => sortDialog.close()}
-								class="absolute top-2 right-2 lg:static lg:top-auto lg:right-auto"
-								><svg
-									xmlns="http://www.w3.org/2000/svg"
-									height="24"
-									viewBox="0 -960 960 960"
-									width="24"
-									><path
-										d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
-									/></svg
-								></button
-							>
-						</div>
-					</dialog>
-				</div>
-				<h3 class="text-2xl">{data.title}</h3>
+				<h3 class="text-lg">{data.title}</h3>
 				<button on:click={() => helpDialog.showModal()}>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 -960 960 960"
-						class="w-6 h-6 fill-megan-700"
+						class="w-4 h-4 fill-megan-700"
 						><path
 							d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342-618l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506-526q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
 						/></svg
@@ -294,29 +244,90 @@
 				</div>
 			{/if}
 		</div>
-	{:else if status === 'error'}
-		<p>Invalid gallery ID</p>
 	{/if}
 </div>
 
-<div>
-	<input
-		type="file"
-		multiple
-		on:change={handleFileChange}
-		bind:this={input}
-		class="hidden invisible"
-		accept="image/*"
-	/>
-	<button
-		on:click={checkIfCanUpload}
-		class="add-btn z-50 bg-megan-600 hover:bg-megan-800 w-14 aspect-square grid place-content-center rounded-full fixed bottom-8 lg:bottom-20 right-4"
-	>
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="w-8 h-8 fill-white"
-			><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" /></svg
+{#if data.status === 'idle'}
+	<div>
+		<input
+			type="file"
+			multiple
+			on:change={handleFileChange}
+			bind:this={input}
+			class="hidden invisible"
+			accept="image/*"
+		/>
+		<button
+			on:click={checkIfCanUpload}
+			class="add-btn z-50 bg-megan-600 hover:bg-megan-800 w-14 aspect-square grid place-content-center rounded-full fixed bottom-8 lg:bottom-20 right-4"
 		>
-	</button>
-</div>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="w-8 h-8 fill-white"
+				><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" /></svg
+			>
+		</button>
+	</div>
+{/if}
+<dialog bind:this={sortDialog} class="absolute">
+	<div
+		class="relative bg-white text-left p-4 rounded-md shadow-lg z-[1001] lg:flex lg:gap-4 items-center"
+	>
+		<p class="text-md text-gray-600">Sort By</p>
+		<div class="bg-gray-100 grid grid-cols-2 gap-2 p-1">
+			<button
+				on:click={() => {
+					if ($sortFieldStore !== 'dateAdded') {
+						sortFieldStore.set('dateAdded');
+						sortDialog.close();
+					}
+				}}
+				class="bg-gray-300 rounded-md py-2 px-4 text-black"
+				class:selected={$sortFieldStore === 'dateAdded'}>Date Added</button
+			>
+			<button
+				on:click={() => {
+					if ($sortFieldStore !== 'dateTaken') {
+						sortFieldStore.set('dateTaken');
+						sortDialog.close();
+					}
+				}}
+				class="bg-gray-300 rounded-md py-2 px-4 text-black"
+				class:selected={$sortFieldStore === 'dateTaken'}>Date Taken</button
+			>
+		</div>
+		<p class="text-md mt-4 lg:mt-0 text-gray-600">Direction</p>
+		<div class="bg-gray-100 grid grid-cols-2 gap-2 p-1">
+			<button
+				on:click={() => {
+					if ($sortDirectionStore !== 'desc') {
+						sortDirectionStore.set('desc');
+						sortDialog.close();
+					}
+				}}
+				class="bg-gray-300 rounded-md py-2 px-4 text-black"
+				class:selected={$sortDirectionStore === 'desc'}>Newest</button
+			>
+			<button
+				on:click={() => {
+					if ($sortDirectionStore !== 'asc') {
+						sortDirectionStore.set('asc');
+						sortDialog.close();
+					}
+				}}
+				class="bg-gray-300 rounded-md py-2 px-4 text-black"
+				class:selected={$sortDirectionStore === 'asc'}>Oldest</button
+			>
+		</div>
+		<button
+			on:click={() => sortDialog.close()}
+			class="absolute top-2 right-2 lg:static lg:top-auto lg:right-auto"
+			><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"
+				><path
+					d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+				/></svg
+			></button
+		>
+	</div>
+</dialog>
 
 <Dialog
 	id="gallryUpload"
@@ -377,7 +388,7 @@
 	{/if}
 </Dialog>
 
-<style lang="postcs gcgasds">
+<style lang="postcs">
 	progress[value] {
 		--color: rgb(147, 31, 62); /* the progress color */
 		--background: rgb(255, 255, 255); /* the background color */
