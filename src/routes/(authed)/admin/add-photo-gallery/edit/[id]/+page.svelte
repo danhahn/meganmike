b45f1d7fb38d96c374b55cfb63f7173b2e1b71dd
Dan <danielhahn@gmail.com>
2024-03-28 15:21:44 -0400

    Fix icon size on small screens in photo gallery

--- src/routes/(authed)/admin/add-photo-gallery/edit/[id]/+page.svelte
+++ src/routes/(authed)/admin/add-photo-gallery/edit/[id]/+page.svelte
@@ -19,8 +19,12 @@
 		...$allPhotos.filter((photo) => photo.disabled !== true),
 		...$allPhotos.filter((photo) => photo.disabled === true)
 	];
+	let innerWidth: number = 0;
 
-	const iconSize = 50;
+	let iconSize = 50;
+	if (innerWidth < 1024) {
+		iconSize = 300;
+	}
 
 	async function deletePhoto(id: string) {
 		const confirm = window.confirm('Are you sure you want to remove this photo?');
@@ -39,13 +43,15 @@
 	}
 </script>
 
+<svelte:window bind:innerWidth />
+
 <Headline>Edit Photo Gallery</Headline>
 
 <div class="grid grid-cols-1 gap-[1px]">
 	<div
-		class="border border-black/20 bg-megan-600 text-white p-1 grid grid-cols-[50px_1fr_1fr_1fr_1fr_auto] gap-4 items-center"
+		class="border border-black/20 bg-megan-600 text-white text-xs lg:text-base p-4 py-2 lg:p-1 grid grid-cols-2 lg:grid-cols-[50px_1fr_1fr_1fr_1fr_auto] gap-1 lg:gap-4 items-center"
 	>
-		<div class="grid place-content-center">
+		<div class="lg:grid place-content-center hidden">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="w-4 h-4 fill-current"
 				><path
 					d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"
@@ -56,7 +62,7 @@
 		<p>Name</p>
 		<p>Date Taken</p>
 		<p>Date Added</p>
-		<p class=" mr-4 p-2">
+		<p class="mr-4 p-2 hidden lg:block">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="w-4 h-4 fill-current"
 				><path
 					d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
@@ -66,14 +72,14 @@
 	</div>
 	{#each photos as photo (photo.id)}
 		<div
-			class={`border border-black/20 bg-white p-1 grid grid-cols-[50px_1fr_1fr_1fr_1fr_auto] gap-4 items-center ${
+			class={`border border-black/20 bg-white p-4 lg:p-1 grid grid-cols-2 lg:grid-cols-[50px_1fr_1fr_1fr_1fr_auto] gap-4 items-center ${
 				photo.disabled ? 'opacity-50' : ''
 			}`}
 		>
 			<img
 				src={`${photo.url}&tr=w-${iconSize},h-${iconSize}`}
 				alt={photo.name}
-				class={`w-[${iconSize}px] h-[${iconSize}px] aspect-square object-cover`}
+				class={`w-[${iconSize}px] h-[${iconSize}px] col-span-2 lg:col-span-1 aspect-square object-cover`}
 				width={iconSize}
 				height={iconSize}
 				loading="lazy"
@@ -89,7 +95,7 @@
 				>
 			{:else}
 				<button
-					class="p-2 rounded-md bg-megan-600 text-white mr-4"
+					class="p-2 rounded-md bg-megan-600 text-white mr-4 col-span-2 lg:col-span-1 flex justify-center items-center gap-2"
 					on:click={() => deletePhoto(photo.id)}
 				>
 					<svg
@@ -100,6 +106,7 @@
 							d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
 						/></svg
 					>
+					<span class="lg:hidden">Delete</span>
 				</button>
 			{/if}
 		</div>
