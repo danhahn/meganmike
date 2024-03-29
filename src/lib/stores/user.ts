62f1cb2c2c5880c57c23daebf5604f408b1e2cc4
Dan <danielhahn@gmail.com>
2024-03-29 16:10:01 -0400

    Add userLikes store

--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 
 export const user = writable(false);
 export const userId = writable<string | undefined>(undefined);
+export const userLikes = writable<string[]>([]);
