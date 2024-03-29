ae89127f33459a8208e927e7762ceb0570834195
Dan <danielhahn@gmail.com>
2024-03-29 16:10:16 -0400

    Add LikeButton and DownloadHelp components to +page.svelte

--- src/routes/(site)/gallery/[id]/[photoId]/+page.svelte
+++ src/routes/(site)/gallery/[id]/[photoId]/+page.svelte
@@ -4,6 +4,9 @@
 	import type { PageData } from './$types';
 	import { gallery } from '$lib/stores/galleryStore';
 	import type { Image } from '$lib/types';
+	import LikeButton from '$lib/components/LikeButton.svelte';
+	import { toggleLike } from '$lib/utils';
+	import DownloadHelp from '$lib/components/DownloadHelp.svelte';
 
 	export let data: PageData;
 
@@ -24,6 +27,8 @@
 	$: innerWidth = 1000;
 
 	$: imageSize = innerWidth > 768 ? 1000 : innerWidth;
+
+	let dialog: HTMLDialogElement;
 </script>
 
 <svelte:head>
@@ -49,17 +54,43 @@
 	</button>
 	<div class="h-full grid place-content-center fixed bg-megan-400 inset-0">
 		{#if currentPhoto !== undefined}
-			<img
-				src={`${currentPhoto.url}&tr=w-${imageSize},h-${imageSize},c-at_least`}
-				alt=""
-				class="max-h-screen shadow-lg shadow-black/40"
-			/>
+			<div class="grid">
+				<img
+					src={`${currentPhoto.url}&tr=w-${imageSize},h-${imageSize},c-at_least`}
+					alt=""
+					class="max-h-screen shadow-lg shadow-black/40 col-start-1 row-start-1"
+				/>
+				<LikeButton id={currentPhoto.id} {toggleLike} likes={currentPhoto.likes} />
+			</div>
 		{/if}
 
 		{#if currentPhoto !== undefined}
-			<Button class="fixed inset-4 top-auto" on:click={() => goto(currentPhoto?.url || '')}
-				>View Larger</Button
-			>
+			<div class="fixed inset-4 top-auto right-auto flex gap-4">
+				<button on:click={() => dialog.showModal()}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 -960 960 960"
+						class="w-8 h-8 fill-megan-50"
+					>
+						><path
+							d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342-618l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506-526q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+						/></svg
+					>
+				</button>
+				<a
+					href={currentPhoto.url}
+					target="_blank"
+					class=" flex gap-2 items-center bg-megan-600/60 text-white px-6 py-2 rounded-full z-50"
+					>Download <svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 -960 960 960"
+						class="w-6 h-6 fill-current"
+						><path
+							d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"
+						/></svg
+					></a
+				>
+			</div>
 		{/if}
 
 		<div class="flex justify-between self-center fixed top-1/2 left-0 right-0">
@@ -102,3 +133,7 @@
 		</div>
 	</div>
 {/if}
+
+<dialog bind:this={dialog} class="bg-transparent">
+	<DownloadHelp close={() => dialog.close()} showCloseButton />
+</dialog>
