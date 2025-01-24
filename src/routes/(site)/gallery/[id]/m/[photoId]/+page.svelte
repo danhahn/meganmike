be5e71fe78fa8f575a0a9cf66eda52e24d634122
Dan Hahn <danielhahn@gmail.com>
2025-01-23 23:16:21 -0500

    feat: Add comment functionality to photo detail page and enhance UI

--- src/routes/(site)/gallery/[id]/m/[photoId]/+page.svelte
+++ src/routes/(site)/gallery/[id]/m/[photoId]/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import Button from '$lib/components/forms/Button.svelte';
 	import { gallery } from '$lib/stores/galleryStore';
-	import type { PageData } from '../$types';
 	export let data: PageData;
 	import LikeButton from '$lib/components/LikeButton.svelte';
-	import { toggleLike } from '$lib/utils';
+	import { addComment, toggleLike } from '$lib/utils';
 	import viewport, { type IntersectionObserverEntry } from '$lib/useViewportAction';
-	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
 
 	let innerWidth = 0;
 	let loading: 'pending' | 'loading' | 'loaded' = 'pending';
@@ -18,7 +16,6 @@
 
 	let photoIndex: number | undefined = undefined;
 	let galleryWrapper: HTMLElement | null = null;
-	let container: HTMLElement | null = null;
 	let imagePositions: {
 		id: string;
 		position: number;
@@ -43,6 +40,8 @@
 		window.scrollTo({ top: 77, behavior: 'instant' });
 	}
 
+	let currentPhotoId: string | undefined = data.photoId;
+
 	function watchScroll() {
 		const scrollLeft = galleryWrapper ? galleryWrapper.scrollLeft : 0;
 		const currentImage = imagePositions.find(
@@ -50,6 +49,7 @@
 		);
 		if (currentImage) {
 			window.history.replaceState(null, '', `/gallery/${data.id}/${currentImage.id}`);
+			currentPhotoId = currentImage.id;
 		}
 	}
 
@@ -84,6 +84,15 @@
 				backToGallery.classList.remove('hide');
 			}
 		}
+	}
+
+	let comment = '🍑';
+
+	async function handleSubmit() {
+		if (!currentPhotoId) return;
+		await addComment(currentPhotoId, comment);
+		console.log('update');
+		comment = '';
 	}
 </script>
 
@@ -131,31 +140,80 @@
 						<div class="z-20 col-start-1 row-start-1 flex justify-end items-center p-6">
 							<LikeButton size="large" id={photo.id} {toggleLike} likes={photo.likes} />
 						</div>
+						{#if photo.comments}
+							<div class="z-20 col-start-1 row-start-1 flex justify-start items-end p-6">
+								<div class="indicator">
+									<span class="indicator-item badge badge-primary font-mono"
+										>{photo.comments.length}</span
+									>
+									<button class="btn" popovertarget={`photo-${photo.id}`}
+										><svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 -960 960 960"
+											class="w-6 h-6 fill-current"
+											><path
+												d="M240-400h480v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM880-80 720-240H160q-33 0-56.5-23.5T80-320v-480q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v720ZM160-320h594l46 45v-525H160v480Zm0 0v-480 480Z"
+											/></svg
+										></button
+									>
+								</div>
+							</div>
+							<div id={`photo-${photo.id}`} popover>
+								<div class="flex justify-between p-2 bg-megan-400">
+									<h3>Comments</h3>
+									<button popovertarget={`photo-${photo.id}`} popovertargetaction="hide"
+										>close</button
+									>
+								</div>
+								<div class="comments">
+									{#each photo.comments.reverse() as { comment, timestamp }}
+										<div class="border-b border-base-300 p-1">
+											<p class="font-bold">{comment}</p>
+											<p class="text-sm opacity-50 text-right">
+												{new Date(timestamp.seconds * 1000).toLocaleString('en-US', {
+													month: '2-digit',
+													day: '2-digit',
+													year: 'numeric',
+													hour: '2-digit',
+													minute: '2-digit'
+												})}
+											</p>
+										</div>
+									{/each}
+								</div>
+								<div class="bg-megan-300 p-2 add-comment">
+									<form
+										on:submit|preventDefault={handleSubmit}
+										class="flex justify-stretch items-center"
+									>
+										<input
+											type="text"
+											class="input w-full focus:outline-none rounded-r-none"
+											placeholder="Add Comment"
+											bind:value={comment}
+										/>
+										<button
+											class="btn btn-primary bg-megan-500 border-megan-700 text-white rounded-l-none"
+											type="submit"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 -960 960 960"
+												class="w-6 h-6 fill-current"
+												><path
+													d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"
+												/></svg
+											>
+										</button>
+									</form>
+								</div>
+							</div>
+						{/if}
 					</div>
 				</div>
 			{/each}
 		</div>
 	{/if}
-</div>
-
-<div class="bg-megan-300 p-2">
-	<form method="POST" class="flex justify-stretch items-center">
-		<input
-			type="text"
-			class="input w-full focus:outline-none rounded-r-none"
-			placeholder="Add Comment"
-		/>
-		<button
-			class="btn btn-primary bg-megan-500 border-megan-700 text-white rounded-l-none"
-			type="submit"
-		>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="w-6 h-6 fill-current"
-				><path
-					d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"
-				/></svg
-			>
-		</button>
-	</form>
 </div>
 
 <style lang="postcss">
@@ -177,5 +235,31 @@
 	#back-to-gallery.hide {
 		/* slide off screen */
 		transform: translateX(calc(-100% - 1rem));
+	}
+
+	[popover] {
+		width: 100vw;
+		min-height: calc(100vh - 64px);
+		height: clamp(10rem, 50vh, 80vh);
+
+		position: relative;
+		margin: 0;
+		margin-top: 77px;
+		padding: 0;
+	}
+
+	[popover] .add-comment {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		width: 100%;
+	}
+
+	[popover] .comments {
+		padding: 0.5rem;
+	}
+	[popover]::backdrop {
+		background-color: rgb(0 0 0 / 50%);
 	}
 </style>
