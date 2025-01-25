ee49190ad6198322ecfc99314365a73cecff94ff
Dan Hahn <danielhahn@gmail.com>
2025-01-25 13:33:45 -0500

    feat: Refactor Comments component to use Comment subcomponent and improve comment sorting

--- src/lib/components/comments/Comments.svelte
+++ src/lib/components/comments/Comments.svelte
@@ -1,35 +1,22 @@
 <script lang="ts">
-	import type { Image } from '$lib/types';
-	import { timeSince } from '$lib/utils';
+	import type { Image, Comment as CommentType } from '$lib/types';
+	import Comment from './Comment.svelte';
 
 	export let photo: Image;
+
+	$: commentsList = photo.comments?.length
+		? photo.comments.sort(
+				(a: CommentType, b: CommentType) => Number(b.timestamp) - Number(a.timestamp)
+			)
+		: [];
 </script>
 
 <div class="comments flex flex-col gap-2 bg-white p-2 overflow-auto">
 	{#if photo.comments === undefined}
 		<p class="text-center p-2">No comments yet</p>
 	{:else}
-		{#each photo.comments.reverse() as { comment, timestamp }}
-			<div class="comment border-b border-base-300 p-1 flex gap-2">
-				<div>
-					<div class="avatar rows">
-						<div class="w-12 rounded-full">
-							<img
-								src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-							/>
-						</div>
-					</div>
-				</div>
-				<div class="grid flex-1">
-					<div class="flex justify-between">
-						<p>UserName</p>
-						<p class="text-sm opacity-50 text-right">
-							{@html timeSince(timestamp)}
-						</p>
-					</div>
-					<p class="font-bold">{comment}</p>
-				</div>
-			</div>
+		{#each commentsList as { comment, timestamp } (timestamp)}
+			<Comment {comment} {timestamp} />
 		{/each}
 	{/if}
 </div>
@@ -39,7 +26,7 @@
 		align-self: end;
 	}
 
-	.comment:last-child {
+	:global(.comment:last-child) {
 		border-bottom: none;
 	}
 </style>
