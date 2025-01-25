bbfa625f16686c58982097126b3303eb1353c13e
Dan Hahn <danielhahn@gmail.com>
2025-01-25 12:27:49 -0500

    refactor: Remove unused styles from +page.svelte for cleaner code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,12 +53,4 @@
 	:global(html) {
 		background-color: theme(colors.megan.50);
 	}
-
-	ul {
-		@apply text-center grid grid-rows-1 gap-2 lg:gap-4 text-xl;
-	}
-
-	li span {
-		@apply text-megan-500 block text-2xl lg:text-3xl mb-1 lg:mb-4;
-	}
 </style>
