c2966cfdc52dacbfeacc11287dacf5650d51df9e
Dan Hahn <danielhahn@gmail.com>
2025-01-24 22:45:52 -0500

    feat: Enhance Comments component with user avatar and timeSince formatting

--- src/lib/components/comments/Comments.svelte
+++ src/lib/components/comments/Comments.svelte
@@ -1,26 +1,45 @@
 <script lang="ts">
 	import type { Image } from '$lib/types';
+	import { timeSince } from '$lib/utils';
 
 	export let photo: Image;
 </script>
 
-<div class="comments flex flex-col gap-2 bg-white p-2">
+<div class="comments flex flex-col gap-2 bg-white p-2 overflow-auto">
 	{#if photo.comments === undefined}
 		<p class="text-center p-2">No comments yet</p>
 	{:else}
 		{#each photo.comments.reverse() as { comment, timestamp }}
-			<div class="border-b border-base-300 p-1">
-				<p class="font-bold">{comment}</p>
-				<p class="text-sm opacity-50 text-right">
-					{new Date(timestamp.seconds * 1000).toLocaleString('en-US', {
-						month: '2-digit',
-						day: '2-digit',
-						year: 'numeric',
-						hour: '2-digit',
-						minute: '2-digit'
-					})}
-				</p>
+			<div class="comment border-b border-base-300 p-1 flex gap-2">
+				<div>
+					<div class="avatar rows">
+						<div class="w-12 rounded-full">
+							<img
+								src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+							/>
+						</div>
+					</div>
+				</div>
+				<div class="grid flex-1">
+					<div class="flex justify-between">
+						<p>UserName</p>
+						<p class="text-sm opacity-50 text-right">
+							{@html timeSince(timestamp)}
+						</p>
+					</div>
+					<p class="font-bold">{comment}</p>
+				</div>
 			</div>
 		{/each}
 	{/if}
 </div>
+
+<style>
+	.comments {
+		align-self: end;
+	}
+
+	.comment:last-child {
+		border-bottom: none;
+	}
+</style>
