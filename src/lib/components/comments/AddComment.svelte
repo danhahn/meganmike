42ce20895ba087849e4f1f7317eca41754432e99
Dan Hahn <danielhahn@gmail.com>
2025-01-25 13:33:26 -0500

    feat: Enhance AddComment component with improved input styling and layout

--- src/lib/components/comments/AddComment.svelte
+++ src/lib/components/comments/AddComment.svelte
@@ -8,12 +8,19 @@
 
 <div class="bg-megan-300 p-2 add-comment">
 	<form on:submit|preventDefault class="flex justify-stretch items-center">
-		<input
-			type="text"
-			class="input w-full focus:outline-none rounded-r-none"
-			placeholder="Add Comment"
-			bind:value={localComment}
-		/>
+		<label class="input input-bordered flex items-center gap-2 w-full">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" class="w-6 h-6 fill-current"
+				><path
+					d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"
+				/></svg
+			>
+			<input
+				type="text"
+				class="grow focus:outline-none rounded-r-none"
+				placeholder="Add A Comment"
+				bind:value={localComment}
+			/>
+		</label>
 		<button
 			class="btn btn-primary bg-megan-500 border-megan-700 text-white rounded-l-none"
 			type="submit"
