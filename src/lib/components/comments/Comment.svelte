1ec6d9982ea49977fa9a157e106763c74de547f2
Dan Hahn <danielhahn@gmail.com>
2025-01-25 13:33:34 -0500

    feat: Add Comment component to display user comments with dynamic timestamp updates

---
+++ src/lib/components/comments/Comment.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { timeSince } from '$lib/utils';
+	import type { Timestamp } from 'firebase/firestore';
+
+	export let comment: string;
+	export let timestamp: Timestamp;
+	import { onMount } from 'svelte';
+
+	let timeDisplay = timeSince(timestamp);
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			timeDisplay = timeSince(timestamp);
+		}, 30000);
+
+		return () => clearInterval(interval);
+	});
+</script>
+
+<div class="comment border-b border-base-300 p-1 flex gap-2">
+	<div>
+		<div class="avatar rows">
+			<div class="w-12 rounded-full">
+				<img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+			</div>
+		</div>
+	</div>
+	<div class="grid flex-1">
+		<div class="flex justify-between">
+			<p>UserName</p>
+			<p class="text-sm opacity-50 text-right">
+				{@html timeDisplay}
+			</p>
+		</div>
+		<p class="font-bold">{comment}</p>
+	</div>
+</div>
