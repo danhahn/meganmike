279113955124eaa9307138a1b3b1b5fa69204129
Dan Hahn <danielhahn@gmail.com>
2025-01-25 17:14:39 -0500

    feat: Update CommentPopover to use reactive photo store and enhance comment submission logic

--- src/lib/components/comments/CommentPopover.svelte
+++ src/lib/components/comments/CommentPopover.svelte
@@ -1,44 +1,47 @@
 <script lang="ts">
+	import { currentPhoto } from '$lib/stores/galleryStore';
 	import type { Image } from '$lib/types';
 	import { addComment } from '$lib/utils';
 	import AddComment from './AddComment.svelte';
 	import Comments from './Comments.svelte';
-	export let photo: Image;
-	export let currentPhotoId: string | undefined;
 
 	let comment: string = '';
 
+	$: photo = $currentPhoto;
+
 	async function handleSubmit(event: CustomEvent<string>) {
 		comment = event.detail;
-		if (!currentPhotoId) return;
+		if (!photo?.id) return;
 		if (!comment) return;
-		await addComment(currentPhotoId, comment);
+		await addComment(photo.id, comment);
 		console.log('update');
 		comment = '';
 	}
 </script>
 
-<div id={`photo-${photo.id}`} popover>
-	<div class="grid grid-rows-[auto_1fr_auto] h-full">
-		<div class="flex justify-between p-2 bg-megan-400">
-			<h3>Comments</h3>
-			<button popovertarget={`photo-${photo.id}`} popovertargetaction="hide">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 -960 960 960"
-					class="w-6 h-6 fill-current"
-					><path
-						d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
-					/></svg
-				>
-			</button>
+{#if photo}
+	<div id="comment-layer" popover>
+		<div class="grid grid-rows-[auto_1fr_auto] h-full">
+			<div class="flex justify-between p-2 bg-megan-400">
+				<h3>Comments</h3>
+				<button popovertarget="comment-layer" popovertargetaction="hide">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 -960 960 960"
+						class="w-6 h-6 fill-current"
+						><path
+							d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+						/></svg
+					>
+				</button>
+			</div>
+			<div class="overflow-auto">
+				<Comments {photo} />
+			</div>
+			<AddComment on:add-comment={handleSubmit} {comment} />
 		</div>
-		<div class="overflow-auto">
-			<Comments {photo} />
-		</div>
-		<AddComment on:add-comment={handleSubmit} {comment} />
 	</div>
-</div>
+{/if}
 
 <style lang="postcss">
 	:global(body:has([popover]:popover-open)) {
