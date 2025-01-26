d184212e446166841435452b0b67fd8e5356120f
Dan Hahn <danielhahn@gmail.com>
2025-01-25 22:25:20 -0500

    feat: Update LikeButton component to support responsive sizes and improve button styling

--- src/lib/components/LikeButton.svelte
+++ src/lib/components/LikeButton.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { userId, userLikes } from '$lib/stores/user';
-	import { sortFieldStore } from '$lib/stores/sortStore';
-
 	export let toggleLike: (id: string) => void;
 	export let id: string;
 	export let likes: number;
 	export let hideCount: boolean = false;
-	export let size: 'small' | 'large' = 'small';
+	export let size: 'sm' | 'lg' = 'lg';
+
+	$: svgSize = size === 'sm' ? 'w-4 h-4' : 'w-6 h-6';
+	$: buttonType = size === 'sm' ? 'btn-ghost' : '';
 </script>
 
 {#if $userId}
@@ -15,12 +16,12 @@
 			{#if likes && !hideCount}
 				<span class="indicator-item badge badge-primary font-mono">{likes}</span>
 			{/if}
-			<button class={`btn btn-${size}`} on:click={() => toggleLike(id)}>
+			<button class={`btn btn-${size} ${buttonType}`} on:click={() => toggleLike(id)}>
 				{#if $userLikes.includes(id)}
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 -960 960 960"
-						class="w-6 h-6 fill-current translate-y-[1px] scale-90 lg:scale-100"
+						class={`${svgSize} fill-megan-600`}
 						><path
 							d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"
 						/></svg
@@ -29,7 +30,7 @@
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 -960 960 960"
-						class="w-6 h-6 fill-black translate-y-[1px] scale-90 lg:scale-100"
+						class={`${svgSize} fill-current`}
 						><path
 							d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"
 						/></svg
