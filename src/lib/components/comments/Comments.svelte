34c255e5f4722f2231f3c9afb605fc5cf623070d
Dan Hahn <danielhahn@gmail.com>
2025-01-29 10:06:36 -0500

    feat: Integrate Firestore for dynamic comment loading with avatar and display name support

--- src/lib/components/comments/Comments.svelte
+++ src/lib/components/comments/Comments.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
 	import type { Image, Comment as CommentType } from '$lib/types';
 	import Comment from './Comment.svelte';
+	import { collectionStore } from 'sveltefire';
+	import { collection, where, query, orderBy } from 'firebase/firestore';
+	import { db, firestore } from '$lib/firebase/firebase';
 
 	export let photo: Image;
 
-	$: commentsList = photo.comments?.length
-		? photo.comments.sort(
-				(a: CommentType, b: CommentType) => Number(b.timestamp) - Number(a.timestamp)
-			)
-		: [];
+	const postsRef = collection(db, 'comments');
+	const q = query(postsRef, where('photoId', '==', photo.id), orderBy('timestamp', 'asc'));
+
+	const comments = collectionStore<CommentType>(firestore, q as any);
 </script>
 
 <div class="comments flex flex-col gap-2 bg-white p-2 overflow-auto">
 	{#if photo.comments === undefined}
 		<p class="text-center p-2">No comments yet</p>
 	{:else}
-		{#each commentsList as { comment, timestamp } (timestamp)}
-			<Comment {comment} {timestamp} />
+		{#each $comments as { comment, timestamp, displayName, avatar } (timestamp)}
+			<Comment {comment} {timestamp} {displayName} {avatar} />
 		{/each}
 	{/if}
 </div>
