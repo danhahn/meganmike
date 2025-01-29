52903e790401a37cbba07963c5398183272d2b06
Dan Hahn <danielhahn@gmail.com>
2025-01-28 22:25:44 -0500

    fix: Update badge styling in TriggerContainer for improved visibility

--- src/lib/components/TriggerContainer.svelte
+++ src/lib/components/TriggerContainer.svelte
@@ -12,9 +12,7 @@
 <div class="comment-trigger">
 	<div class="indicator">
 		{#if itemsCount}
-			<span
-				class="indicator-item p-1 badge badge-secondary font-mono"
-				class:scale-50={size === 'sm'}
+			<span class="indicator-item badge badge-primary font-mono" class:scale-50={size === 'sm'}
 				><span class="text-[0.6rem]" class:hidden={size === 'sm'}>{itemsCount}</span></span
 			>
 		{/if}
