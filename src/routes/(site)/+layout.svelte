ad2cdeaed432aa8d7732bf67fc06fab8302e5ee5
Dan Hahn <danielhahn@gmail.com>
2025-01-29 10:06:57 -0500

    feat: Wrap layout in FirebaseApp component for improved Firebase integration

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import Background from '$lib/components/Background.svelte';
 	import Navigation from '$lib/components/Navigation.svelte';
+	import { auth, firestore, storage } from '$lib/firebase/firebase';
+	import { FirebaseApp } from 'sveltefire';
 </script>
 
-<Background>
-	<div class="grid grid-rows-[auto_1fr] h-full 💒">
-		<div>
-			<Navigation />
+<FirebaseApp {auth} {firestore} {storage}>
+	<Background>
+		<div class="grid grid-rows-[auto_1fr] h-full 💒">
+			<div>
+				<Navigation />
+			</div>
+			<div>
+				<slot />
+			</div>
 		</div>
-		<div>
-			<slot />
-		</div>
-	</div>
-</Background>
+	</Background>
+</FirebaseApp>
