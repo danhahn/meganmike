318cc303595902ca2ac1b6832774630d2c33afc9
Dan Hahn <danielhahn@gmail.com>
2025-01-31 10:49:52 -0500

    refactor: Remove userStore function and related imports from Firebase module

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -1,35 +1,5 @@
-import { onAuthStateChanged, signInWithPopup, type User } from 'firebase/auth';
-import { writable } from 'svelte/store';
+import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from './firebase';
-
-/**
- * @returns a store with the current firebase user
- */
-function userStore() {
-	let unsubscribe: () => void;
-
-	if (!auth || !globalThis.window) {
-		console.warn('Auth is not initialized or not in browser');
-		const { subscribe } = writable<User | null>(null);
-		return {
-			subscribe
-		};
-	}
-
-	const { subscribe } = writable<User | null>(auth?.currentUser ?? null, (set) => {
-		unsubscribe = onAuthStateChanged(auth, (user) => {
-			set(user);
-		});
-
-		return () => unsubscribe();
-	});
-
-	return {
-		subscribe
-	};
-}
-
-export const user = userStore();
 
 export async function functionSignInWithGoogle() {
 	await signInWithPopup(auth, provider);
